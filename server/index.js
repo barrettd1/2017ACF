018ff115731ac340896ac82ca502453f272094d7
Danyelle B <barrettd1@hawkmail.newpaltz.edu>
2017-10-03 11:33:04 -0400

    added necessary pages and elements

---
+++ server/index.js
@@ -0,0 +1,15 @@
+const http = require("http");
+const handler = require("./httpHandler.js");
+
+const server = http();
+
+server.use("/json", function(req, res, next){
+    res.send({ happy: "Yom Kippur"})
+    next();
+});
+
+server.use("/old", handler.main);
+
+server.listen(3000);
+
+console.log("http://localhost:3000");
